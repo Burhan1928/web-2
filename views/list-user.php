d18428adf7f9ccefed44b3a0266be10f52debe85
burhan kallamullah <burhankallamullah12@gmail.com>
2025-06-23 22:46:25 +0700

    Update list-user.php

--- views/list-user.php
+++ views/list-user.php
@@ -73,7 +73,7 @@ if(isset($_SESSION['alert']))
                     </div>
                     <div class="sb-sidenav-footer">
                         <div class="small">Logged in as:</div>
-                        Zaidaan A Dzihnie
+                        burhan kallamullah
                     </div>
                 </nav>
             </div>
@@ -141,7 +141,7 @@ if(isset($_SESSION['alert']))
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; DANZ Company <?= date('Y') ?></div>
+                            <div class="text-muted">Copyright &copy; burhan Company <?= date('Y') ?></div>
                             <div>
                                 <a href="#">Privacy Policy</a>
                                 &middot;
